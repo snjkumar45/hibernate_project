c1ca5905d31b9d326ad4630c7fca4c7ab78acbaa
sanjay <snjkumar45@outlook.com>
2020-12-01 10:34:55 +0530

    configuration and call interface session factory

--- hibernare.tut/src/main/java/com/hibernate/tut/App.java
+++ hibernare.tut/src/main/java/com/hibernate/tut/App.java
@@ -1,5 +1,8 @@
 package com.hibernate.tut;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "project started" );
+        Configuration cfg=new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory=cfg.buildSessionFactory();
+       // factory.getCurrentSession();
+        System.out.println(factory);
     }
 }
